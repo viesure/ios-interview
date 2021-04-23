a8c8a2a292191530d07613cc4df11295b3e53881
Primoz <primoz.cuvan@gmail.com>
2021-04-23 11:23:49 +0200

    create ArticlesView

---
+++ viesure_iOS_interview/View/ArticlesView.swift
@@ -0,0 +1,21 @@
+//
+//  ArticlesView.swift
+//  viesure_iOS_interview
+//
+//  Created by Primoz Cuvan on 23.04.21.
+//  Copyright © 2021 Primoz Cuvan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ArticlesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ArticlesView_Previews: PreviewProvider {
+    static var previews: some View {
+        ArticlesView()
+    }
+}
